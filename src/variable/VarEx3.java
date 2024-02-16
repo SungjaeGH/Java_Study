31f0118b85ad190a2bdb8a82ab391a534cb7729d
SungJae <dtc13579@gmail.com>
2024-02-16 19:25:32 +0900

    feat: 변수 강의 예제 3문제 추가

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longTest = 10000000000L;
+        boolean booleanTest = true;
+
+        System.out.println("long result : " + longTest);
+        System.out.println("boolean result : " + booleanTest);
+    }
+}
